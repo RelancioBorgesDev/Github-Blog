320239c1b0b3cf708252364c41c84bfe61617156
RelancioBorgesDev <relancio.dev@gmail.com>
2022-12-26 16:59:03 -0300

    Adicionado validacao da search bar

--- src/pages/Blog/components/SearchForm/index.tsx
+++ src/pages/Blog/components/SearchForm/index.tsx
@@ -1,13 +1,35 @@
-import { FormContainer, FormHeader, FormInput } from './style'
+import { FormContainer, FormHeader, FormInput } from "./style";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+const searchFormSchema = z.object({
+  query: z.string(),
+});
+type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
 export default function SearchForm() {
+  const { register, handleSubmit, reset } = useForm<SearchFormInputs>({
+    resolver: zodResolver(searchFormSchema),
+  });
+
+  async function retornaQuery(data: SearchFormInputs) {
+    console.log(data);
+
+    reset();
+  }
+
   return (
-    <FormContainer>
-        <FormHeader>
-            <h2>Publicações</h2>
-            <span>6 publicações</span>
-        </FormHeader>
-        <FormInput type="text" placeholder='Buscar conteúdo' />
+    <FormContainer onSubmit={handleSubmit(retornaQuery)}>
+      <FormHeader>
+        <h2>Publicações</h2>
+        <span>6 publicações</span>
+      </FormHeader>
+      <FormInput
+        type='text'
+        placeholder='Buscar conteúdo'
+        {...register("query")}
+      />
     </FormContainer>
-  )
+  );
 }
